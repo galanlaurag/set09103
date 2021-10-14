0695d1108f4465c233cbb567470629faf553d670
Laura Galan <40439289@live.napier.ac.uk>
2021-10-14 09:33:29 +0100

    Added

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello Napier'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+	return render_template('conditional.html', name=name)
 
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug =True)
